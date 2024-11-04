0e31e064481d87fd195d068b758f7375f6196e95
Bengt-Erik Fröberg <bengt-erik.froeberg@redpill-linpro.com>
2024-11-04 16:07:10 +0100

    Batching

--- src/main/java/net/benfro/lab/reactor/S02_mono/MonoDemo.java
+++ src/main/java/net/benfro/lab/reactor/S02_mono/MonoDemo.java
@@ -2,19 +2,39 @@ package net.benfro.lab.reactor.S02_mono;
 
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
+import net.benfro.lab.reactor.common.DefaultSubscriber;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 public class MonoDemo {
 
-    Mono<String> getMono(String string) {
-        return Mono.just(string);
+    /**
+     * Mono can contain 0 or 1 object
+     */
+    public static void main(String[] args) {
+
+        Mono.just("Hey world")
+            .subscribe(DefaultSubscriber.of());
+
+        Mono.fromSupplier(() -> "Hey ho!")
+            .subscribe(DefaultSubscriber.of());
+
+        // Callbacks on success and failure
+        Mono.fromCallable(() -> adder(List.of(1, 2, 3)))
+            .subscribe(i -> log.info(i + ""),
+                err -> log.error("error {}", err.getMessage()),
+                () -> log.info("completed"));
+
+        // Error handling
+        Mono.fromCallable(() -> adder(List.of(1, 2, 3)) / 0)
+            .subscribe(i -> log.info(i + ""),
+                err -> log.error("error {}", err.getMessage()),
+                () -> log.info("completed"));
+
     }
 
-    Mono<String> getEmptyMono() {
-        return Mono.empty();
-    }
-
-    int adder(List<Integer> list) {
+    static int adder(List<Integer> list) {
         return list.stream().mapToInt(i -> i).sum();
     }
 
--- src/main/java/net/benfro/lab/reactor/S03_flux/FluxDemo.java
+++ src/main/java/net/benfro/lab/reactor/S03_flux/FluxDemo.java
@@ -1,14 +1,19 @@
 package net.benfro.lab.reactor.S03_flux;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.stream.Stream;
 
 import net.benfro.lab.reactor.common.DefaultSubscriber;
+import net.benfro.lab.reactor.common.RunUtilities;
 import net.benfro.lab.reactor.common.generator.GenerateUtility;
 import reactor.core.publisher.Flux;
 
 public class FluxDemo {
 
+    /**
+     * Flux can contain 0..n objects
+     */
     public static void main(String[] args) {
 
         Flux.fromStream(Stream.of(1, 2, 3))
@@ -27,5 +32,33 @@ public class FluxDemo {
             .map(i -> GenerateUtility.faker.artist().name())
             .log("log-artists")
             .subscribe(DefaultSubscriber.of());
+
+        Flux.interval(Duration.ofMillis(500))
+            .take(5)
+            .map(i -> GenerateUtility.faker.commerce().productName())
+            .subscribe(DefaultSubscriber.of());
+
+        Flux.merge(just10(), just3())
+            .subscribe(DefaultSubscriber.of("QQQ"));
+
+        Flux.just(1, 2, 3)
+            .concatWith(just10())
+            .subscribe(DefaultSubscriber.of("Q"));
+
+        Flux.combineLatest(just10(), just3(), (a, b) -> a + b)
+            .subscribe(DefaultSubscriber.of("XXX"));
+
+        Flux.combineLatest(just3(), just10(), (a, b) -> a + b)
+            .subscribe(DefaultSubscriber.of("YYY"));
+
+        RunUtilities.sleep(20);
+    }
+
+    static Flux<Integer> just10() {
+        return Flux.range(1, 10);
+    }
+
+    static Flux<Integer> just3() {
+        return Flux.just(1, 2, 3);
     }
 }
